f297e851380c0044d0f0b64a2e472dc50a058bcd
MuammerSuslu <musu1221@student.harmonytx.org>
2022-08-19 08:52:36 -0500

    This is first commit

---
+++ MyFirstClass.java
@@ -0,0 +1,27 @@
+public class MyFirstClass
+{
+//    After you create your first and class
+//    you have to create your main method to run commands
+//    in order to se shortcut you tpye main and press <tab>
+    public static void main(String[] args) {
+//        to print out something to the screen you type system.out.println("your text here");
+//        each line of the code must end with semicolown. ;
+//        shortcut or System.out.println() is sout and <tab>
+//                this double // means this is not a part of your code it is just a comment line to explain or taking note
+//
+//        this is single line comment but if you need multiple line
+
+        /*
+            this is multiple line comment untill the
+            end
+        of
+        the comment
+        your can type
+        */
+        System.out.println("My First Line Of Code");
+//        another way to print on screen with
+
+
+    }
+
+}
