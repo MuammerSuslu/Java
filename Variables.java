dab47b419d88299286300c7646dfd9f4ecaf75f9
MuammerSuslu <musu1221@student.harmonytx.org>
2022-08-19 09:16:09 -0500

    Variables for java.

--- Variables.java
+++ Variables.java
@@ -1,4 +1,35 @@
 public class Variables
 {
+    public static <boolen> void main(String[] args)
+    {
+//        DECLARATION OF Variables
+//        DataType VariablesName = Value
+        String name = "Muammer";     // Text or String
+        int myNum = 5;               // Integer (whole number)
+        float myfloatnum = 5.99f;    // Floating oint number
+        char myletter = 'D';         // charecter, you can use numbers from Iscii table to define.
+        boolen mybool = true;        // Boolean
+        Byte smallNumber = 3;        // stares whole numbers from -128 to 127
+        short shortNumber = 4;       // Stores whole numbers from -32,768 to 32,767
+        long longNumber = 123456789; // Stores whole numbersform -9223,372,036,854,775.808 to 9223,372,036,854,775.807
+        double doubleNumber = 12.34; // Stores fractional number. Sufficent for staring 15 decial digits
+        // RULES FOR VARIABLES AND DAE TYPES
+
+        //  You can not give reserved names for a variable like class etc...
+        //  it should not start from a number
+        //  start always with lower case, you can start with upper case but not preferable way
+        //  primitives are byte short long int float double char boolean
+        //  float ends with f
+        //  double end with a d but it is not supposed to be
+        //  long end with a l but it is not supposed to be
+        //  char has only one character within single quote
+        // boolean has only true and false as value
+
+
+
+
+
+        System.out.println();
+    }
 
 }
